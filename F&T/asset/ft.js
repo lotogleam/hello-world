3359f2bc48f42b0d4e17bfeafe3da7abb5020c4a
Gayathri <mithuna.gayi@gmail.com>
2019-09-23 19:44:08 +0530

    modified some changes

---
+++ F&T/asset/ft.js
@@ -0,0 +1,11 @@
+  var count = 0;
+        var counts= 0;
+        function facebook() {
+            count =count+1;
+            document.getElementById("add").innerHTML = count;
+        };
+        function twitter() {
+            counts=counts+1;
+            document.getElementById("clicks").innerHTML = counts;
+           
+        };
