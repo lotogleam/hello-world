c32e95164897011a77c24e8ae45a1e8d7926142c
Jenifer777 <jeniferbabu2016@gmai.com>
2019-09-25 20:37:06 +0530

    modified the count project

---
+++ jenifer/script/count.js
@@ -0,0 +1,12 @@
+
+             var count=0;
+               function counter(){
+                   count=count+1;
+                   document.getElementById("fb").innerHTML=count;
+               }
+               var count1=0;
+               function counter1(){
+                   count1=count1+1;
+                   document.getElementById("tw").innerHTML=count1;
+               }
+        
