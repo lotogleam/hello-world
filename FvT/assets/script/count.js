72548f32bcb2e62ce20042f21738f9e4328883b4
Raksheeth <raksheethrakshu@gmail.com>
2019-09-26 10:55:34 +0530

    Have made the page responsive. Added -moz- and -ms- counterpart

--- FvT/assets/script/count.js
+++ FvT/assets/script/count.js
@@ -3,14 +3,14 @@ var countButton = document.getElementsByClassName("count-button")[0];
 var displayCount = document.getElementsByClassName("displayCount")[0];
 function vote(){
   counter=counter+1;
-  displayCount.innerHTML = counter;
+  displayCount.innerHTML = "Count:" + counter;
 }
 var count=0;
 var countButton1 = document.getElementsByClassName("count-button")[1];
 var displayCount1 = document.getElementsByClassName("displayCount")[1];
 function vote1(){
   count=count+1;
-  displayCount1.innerHTML = count;
+  displayCount1.innerHTML = "Count:" + count;
 }
 function reset(){
     count=0;
