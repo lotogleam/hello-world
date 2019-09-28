876f397d99b37921546b99711fc277c6d5cbe77d
Jenifer777 <jeniferbabu2016@gmai.com>
2019-09-28 19:16:40 +0530

    small chages in code

--- jenifer/script/sum.js
+++ jenifer/script/sum.js
@@ -1,9 +1,13 @@
 
   
- function sum()
+function sum1()
 {
     var n= document.getElementsByClassName("user-name")[0];
-    add=n.value*(n.value-1)/2;
-   var elem=document.getElementById("add")
-   elem.innerHTML="the value of sum is " +add;
+    add=0;
+     for(i=0;i<=n.value;i++) 
+     {
+         add=add+i;
+     } 
+     var elem=document.getElementById("add");
+   elem.innerHTML="the value of sum is " +add; 
 }
