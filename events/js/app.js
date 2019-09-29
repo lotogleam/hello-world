dc49f31b480f589bdcad5cb649cd99fd76bbd6a8
Gupta <pankgupta@deloitte.com>
2019-09-29 12:44:34 +0530

    - added events with keyboard

---
+++ events/js/app.js
@@ -0,0 +1,21 @@
+function validateClick() {
+  console.log(event);
+  if(event.which === 83) {
+    getAndSummate();
+  }
+}
+
+function getAndSummate() {
+  var elem = document.getElementsByName("upto")[0],
+    sum = performAddition(parseInt(elem.value));
+
+  document.getElementsByClassName("result-wrapper")[0].innerHTML = '<p class="text-center">'+sum+'</p>';
+}
+
+function performAddition(upto) {
+  var sum = 0;
+  if(upto>0) {
+    sum = upto*(upto+1)/2;
+  }
+  return sum;
+}
