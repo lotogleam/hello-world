6f4ce67726ecb8d2b65029ae419a30d954b89cbb
Jenifer777 <jeniferbabu2016@gmai.com>
2019-10-07 21:18:44 +0530

    small modification

---
+++ jenifer/script/spa.js
@@ -0,0 +1,14 @@
+var details=
+{
+   jeni:"hello,I am Jenifer. we working on SPA",
+   gayu:"hello,I am gayathri. we working on SPA",
+   priya:"hello,I am priya. we working on SPA",
+   pankaj:"hello,I am pankaj. we working on SPA"
+}
+  function showdetails(x)
+{
+ var elem = document.getElementById("my");
+ elem.innerHTML=details[x];
+}
+
+   
