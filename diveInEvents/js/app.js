f2e8be0ea985b89cca9236c08d1771a4a5505239
Gupta <pankgupta@deloitte.com>
2019-10-13 12:17:27 +0530

    - understanding event bubbles class

---
+++ diveInEvents/js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".red").click(function(){
+    alert("red")
+  });
+  $(".blue").click(function(){
+    alert("blue");
+  });
+  $(".green").click(function(){
+    alert("green");
+  });
+});
