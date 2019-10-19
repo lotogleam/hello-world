640070a59e3bc43462054a0e49cb5d147530edb5
Gupta <pankgupta@deloitte.com>
2019-10-19 12:38:55 +0530

    - wip loclastorage

--- storage/js/app.js
+++ storage/js/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  initialize();
   $("form[name='userInfo']").on("submit", function(e) {
     // Stop the default action of the submit button.
     e.preventDefault();
@@ -10,6 +11,54 @@ $(document).ready(function() {
       password: form.find(".password").val()
     };
 
-    console.log(valueToBeSaved);
-  })
+    // Check if already we have users saved, if yes, add the new user to the existing list
+    // Else, create an array with current value
+    let userList = getUserList();
+    userList.push(valueToBeSaved);
+    localStorage.setItem('userList', JSON.stringify(userList));
+    if(userList.length) {
+      $(".existing-users ol").append(generateUserTemplate(valueToBeSaved));
+    }
+    else {
+      $(".existing-users").html('<ol>' + generateUserTemplate(valueToBeSaved) +'</ol>');
+    }
+    form[0].reset();
+  });
+
+  
 });
+
+function initialize() {
+  let existingUsers = getUserList();
+  if(existingUsers.length) {
+    let template = generateUserTemplate();
+    $(".existing-users").html(template);
+  }
+}
+
+function generateUserTemplate(user) {
+  let template = '';
+  if(!user) {
+    template += '<ol>';
+    let userList = getUserList();
+    for(let i =0; i<userList.length; i++) {
+      let user = userList[i];
+      template += '<li>' + user.name + '(' + user.email + ')</li>';
+    }
+    template += '</ol>';
+  }
+  else {
+    template += '<li>' + user.name + '(' + user.email + ')</li>';
+  }
+
+  return template;
+}
+
+function getUserList() {
+  let existingValues = [];
+  if(localStorage.getItem('userList')) {
+    existingValues = JSON.parse(localStorage.getItem('userList'));
+  }
+
+  return existingValues;
+}
